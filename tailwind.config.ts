9206e722bf3b61eff3b3174bb40fa823c08be87e
SYL | Lucas Gomes Gato <lucas.gomes@syl.com.br>
2026-09-04 10:37:55 -0300

    config: setup next.js 15 config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
